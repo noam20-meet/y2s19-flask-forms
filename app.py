c36758fb0480428056712087eb3caedaf7e9529c
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:28:56 -0400

    forms lab

--- app.py
+++ app.py
@@ -6,11 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', students=query_all())
 
-@app.route('/student/<int:student_id>')
+@app.route('/student/<int:student_id>', methods=['GET', 'POST'])
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
 #Create an '/add' route here:
+@app.route('/add', methods=['GET', 'POST'])
+def add_student_route():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        name = request.form['student_name']
+        year = request.form['student_year']
+
+
+        add_student(name, year)        
+        return render_template('add.html',
+            n = name,
+            y = year)
+
+       
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
